6642074b4b65482d13e1e25398c190310127804c
Tushar Pawar <34652100+tusharpawar611@users.noreply.github.com>
2021-06-09 23:57:42 +0530

    BT2

---
+++ BinaryTrees2/mirrorBinaryTree.java
@@ -0,0 +1,42 @@
+// Mirror Binary Tree
+// Send Feedback
+// For a given Binary Tree of type integer, update it with its corresponding mirror image.
+
+/*
+	
+	Following is the structure used to represent the Binary Tree Node
+
+	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+*/
+
+public class Solution {
+
+	public static void mirrorBinaryTree(BinaryTreeNode<Integer> root){
+		//Your code goes here
+        if(root==null){
+            return;
+        }
+        BinaryTreeNode<Integer> temp=null;
+        // BinaryTreeNode<Integer> n2;
+        temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+        
+        mirrorBinaryTree(root.left);
+        mirrorBinaryTree(root.right);
+        
+        
+	}
+	
+}
