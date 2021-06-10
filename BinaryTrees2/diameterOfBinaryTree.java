e955c114a5f7e9bccd4235a895f79259ea30f2a1
Tushar Pawar <34652100+tusharpawar611@users.noreply.github.com>
2021-06-10 23:44:27 +0530

    bt2

---
+++ BinaryTrees2/diameterOfBinaryTree.java
@@ -0,0 +1,42 @@
+// Diameter Of Binary Tree
+// Send Feedback
+// For a given Binary of type integer, find and return the ‘Diameter’.
+// Diameter of a Tree
+// The diameter of a tree can be defined as the maximum distance between two leaf nodes.
+// Here, the distance is measured in terms of the total number of nodes present along the path of the two leaf nodes, including both the leaves.
+
+
+/*
+	
+	Following is the structure used to represent the Binary Tree Node
+
+	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+*/
+
+public class Solution {
+
+    class BT{
+        public int height;
+        public int ld;
+        public int rd;
+    
+	}
+	public static int diameterOfBinaryTree(BinaryTreeNode<Integer> root){
+		//Your code goes here
+        return 0;
+	}
+	
+}
+
+ 
